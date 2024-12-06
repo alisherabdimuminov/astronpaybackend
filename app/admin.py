9b3e87d866b7725252accae5da93907e272aa502
alishercpp <alisher.abdimuminov.2005@gmail.com>
2024-12-06 16:46:44 +0500

    bug fix

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Transaction
+
+
+@admin.register(Transaction)
+class TransactionModelAdmin(admin.ModelAdmin):
+    list_display = ["id", "appid", "state", "created"]
+
